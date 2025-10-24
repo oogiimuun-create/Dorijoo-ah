f9ee5ba93abc1300116e4a48d8802ae2fdb1c5cc
Hem1x <oogiimuun@gmail.com>
2025-10-24 20:29:12 +0800

    first commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,29 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <div className="">something</div>
+    <div className="w-full min-h-screen py-20 px-80">
+      <div className="text-[40px] ">
+        <h3 className="font-bold">Hi, I’m Hem1x 👋</h3>
+        <div className="grid grid-cols-2 gap-50">
+          <p className="text-[32px]">
+            I'm a full stack developer (React.js & Node.js) with a focus on
+            creating
+            <br />
+            (and occasionally designing) exceptional digital experiences that
+            are
+            <br />
+            fast, accessible, visually appealing, and responsive. Even though I
+            have
+            <br />
+            been creating web applications for over 7 years, I still love it as
+            if
+            <br />
+            it was something new.
+          </p>
+          <Image width={400} height={400} alt="lol" src="/image/profile.jpg" />
+        </div>
+      </div>
 
       <Link href={"/about"}></Link>
       <Link href={"/Contact"}></Link>
